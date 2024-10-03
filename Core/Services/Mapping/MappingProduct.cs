88e8aa050a54ab0d86cdab0eb1cae0074219612c
Hany148 <hanymahrous046@gmail.com>
2024-10-03 14:32:14 +0300

    Part 02 Product Service & Service Manager

---
+++ Core/Services/Mapping/MappingProduct.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Mapping
+{
+    public class MappingProduct : Profile
+    {
+        public MappingProduct()
+        {
+            CreateMap<Product , ProductDTO >()
+                .ForMember(pd=>pd.BrandName , option=>option.MapFrom(p=>p.productBrand.Name))
+                .ReverseMap();
+
+            CreateMap<ProductBrand, ProductBrandDTO>()
+                .ReverseMap();
+
+            CreateMap<ProductTypeDTO, ProductTypeDTO>()
+                .ReverseMap();
+        }
+    }
+}
---
+++ Core/Services/ProductService.cs
@@ -0,0 +1,78 @@
+﻿global using Domain.Contracts.IUnitOfWork;
+global using Shared.DTO;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Abstractions
+{
+    public class ProductService : IProductService
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        private readonly IMapper _mapper;
+
+        public ProductService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductBrandDTO>> GetProductsBrandDTOAsync()
+        {
+            // 1. Retrive all ProductBrand by using IUnitOfWork interface
+            var productBrands = await  _unitOfWork.GetRepository<ProductBrand, int>().GetAllAsync(true);
+
+            // 2. Mapping to ProductBrandDTO By using package of Automapper
+            var productBrandsDto = _mapper.Map<IEnumerable<ProductBrandDTO>>(productBrands);
+
+            // 3. Return
+
+            return productBrandsDto;
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProductsDTOAsync()
+        {
+            // 1. Retrive all ProductBrand by using IUnitOfWork interface
+            var products = await _unitOfWork.GetRepository<Product, int>().GetAllAsync(true);
+
+            // 2. Mapping to ProductBrandDTO By using package of Automapper
+            var productsDto = _mapper.Map<IEnumerable<ProductDTO>>(products);
+
+            // 3. Return
+
+            return productsDto;
+        }
+
+        public async Task<ProductDTO> GetProductsDTOByIdAsync(int id)
+        {
+            // 1. Retrive all ProductBrand by using IUnitOfWork interface
+            var product = await _unitOfWork.GetRepository<Product, int>().FindByIdAysnc(id);
+
+            // 2. Mapping to ProductBrandDTO By using package of Automapper
+            var productDto = _mapper.Map<ProductDTO>(product);
+
+            // 3. Return
+
+            return productDto;
+        }
+
+        public async Task<IEnumerable<ProductTypeDTO>> GetProductsTypeDTOAsync()
+        {
+            // 1. Retrive all ProductBrand by using IUnitOfWork interface
+            var productTypes = await _unitOfWork.GetRepository<ProductBrand, int>().GetAllAsync(true);
+
+            // 2. Mapping to ProductBrandDTO By using package of Automapper
+            var productTypesDto = _mapper.Map<IEnumerable<ProductTypeDTO>>(productTypes);
+
+            // 3. Return
+
+            return productTypesDto;
+        }
+    }
+}
