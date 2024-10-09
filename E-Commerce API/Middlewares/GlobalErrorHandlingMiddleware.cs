c1ee8320607e10ebf594217dcfceb2f60922b55a
Hany148 <hanymahrous046@gmail.com>
2024-10-09 22:56:54 +0300

    Part 08 Custom Not Found End Point Response

--- E-Commerce API/Middlewares/GlobalErrorHandlingMiddleware.cs
+++ E-Commerce API/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -7,9 +7,9 @@ namespace E_Commerce_API.Middlewares
     public class GlobalErrorHandlingMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly ILogger _logger ;
+        private readonly ILogger _logger;
 
-        public GlobalErrorHandlingMiddleware(ILogger logger , RequestDelegate next)
+        public GlobalErrorHandlingMiddleware(ILogger logger, RequestDelegate next)
         {
             _logger = logger;
             _next = next;
@@ -21,26 +21,33 @@ namespace E_Commerce_API.Middlewares
             {
                 await _next(httpContext); // _next.Invoke(httpContext)
 
+                if (httpContext.Response.StatusCode == (int)HttpStatusCode.NotFound)
+                {
+                   await HandleNotFountPointAsync(httpContext);
+
+                }
+
             }
             catch (Exception exception)
             {
                 _logger.LogError($"Something Went Wrong {exception}");
 
-                await HandleExceptionAsync(httpContext , exception);
+                await HandleExceptionAsync(httpContext, exception);
             }
         }
+
 
         private async Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
         {
             // set status code of response to 500
-            httpContext.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
             // set content type of response to Application/json
             httpContext.Response.ContentType = "application/json";
 
-          
+
             httpContext.Response.StatusCode = exception switch
-            { 
+            {
                 NotFoundException => (int)HttpStatusCode.NotFound,
                 _ => (int)HttpStatusCode.InternalServerError
             };
@@ -54,10 +61,28 @@ namespace E_Commerce_API.Middlewares
                 ErrorMessage = exception.Message
             }.ToString();
 
-          
+
 
             await httpContext.Response.WriteAsync(resonse);
 
         }
+
+
+
+        private async Task HandleNotFountPointAsync(HttpContext httpContext)
+        {
+            httpContext.Response.ContentType = "application/json";
+
+            var Response = new ErrorDetails()
+            {
+                StatusCode = httpContext.Response.StatusCode,
+                ErrorMessage = $"Error Not Fount this Path {httpContext.Request.Path}"
+
+            }.ToString();
+
+            await httpContext.Response.WriteAsync(Response);
+
+        }
+
     }
 }
