699d6ccdda22c8a9616a572b668aa2dad9c3d93b
Hany148 <hanymahrous046@gmail.com>
2024-10-22 12:33:44 +0300

    Part 04 Order Profile {Mapping Profile}

--- Core/Services/Mapping/OrderMapping.cs
+++ Core/Services/Mapping/OrderMapping.cs
@@ -11,9 +11,30 @@ namespace Services.Mapping
 {
     public class OrderMapping : Profile
     {
-        public OrderMapping() 
+        public OrderMapping()
         {
-            CreateMap<OrderEntity, OrderDTO>();
+
+            CreateMap<OrderEntity, OrderDTO>()
+                .ForMember(od => od.paymentStatus, option => option.MapFrom(o => o.paymentStatus.ToString()))
+                .ForMember(od => od.DeliveryMethod, option => option.MapFrom(o => o.DeliveryMethod.ShortName))
+                .ForMember(od => od.Total, option => option.MapFrom(o => ( o.SubTotal + o.DeliveryMethod.Price)))
+
+                .ReverseMap()
+                .ForMember(od => od.paymentStatus, option => option.MapFrom(o => Enum.Parse<OrderPaymentStatus>(o.paymentStatus)))
+;
+
+
+            CreateMap<Address, AddressDTO>().ReverseMap();
+
+            CreateMap<OrderItem, OrderItemDTO>()
+                .ForMember(OID => OID.ProductId , option=> option.MapFrom(OI => OI.Product.ProductId))
+                .ForMember(OID => OID.ProductName , option=> option.MapFrom(OI => OI.Product.ProductName))
+                .ForMember(OID => OID.PicutreUrl , option=> option.MapFrom(OI => OI.Product.PicutreUrl))
+                .ReverseMap();
+
+
+            CreateMap<DeliveryMethod, DeliveryMethodDTO>().ReverseMap();
+
         }
     }
 
